a7c88aed4fed39645eda41d855be0c50b9c4cf1f
Pranav V <100434347+jainvpranav@users.noreply.github.com>
2023-12-20 17:32:36 +0530

    Delete assets/videos/js directory

--- assets/videos/js/supa.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-const supabaseurl='https://zknxbvmxrrhrrybszbny.supabase.co'
-const supabasekey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inprbnhidm14cnJocnJ5YnN6Ym55Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5NTIxNjMsImV4cCI6MjAxNzUyODE2M30.uoHWshWJ4x0uyKu-7v-eOABGmsTJm3tRo_9wNGt8jdU'
-
-export const supabase = createClient(supabaseurl, supabasekey);
