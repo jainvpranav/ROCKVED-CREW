2a843b046d06dbab1d1ffb6bd427b0137ca6f431
NavaprettamN <88848234+NavaprettamN@users.noreply.github.com>
2023-12-20 16:59:00 +0530

    Create Scripts.cs

---
+++ VR-Rocket-Simulation/Scripts.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocketScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    float accelaration = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        accelaration += 0.004f;
+        rb.velocity = new Vector3(0, accelaration, 0);
+    }
+}
